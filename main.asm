7bf5b89d1f7d1879f1a8915cb6334b856937bc1c
emenzen <50517871+emenzen@users.noreply.github.com>
2019-05-21 21:38:16 -0300

    Update main.asm
    versão com as 3 telas, inicio, configuração e tela do jogo

--- main.asm
+++ main.asm
@@ -1,14 +1,31 @@
 .model small
 .stack 100h
 .data
-	vetNumeros			db '0','1','2','3','4','5','6','7','8','9'
-	tituloConfigIniciAL db "Matriz de Navios"
-	textoConfig 		db "Digite a posicao do"
+    	nomeJogo1 		db "Batalha"
+    	nomeJogo2 		db "Naval"
+	textAutores		db "Autores:"
+    	nomeAnderson 		db "Anderson Imperatori"    
+    	nomeEduardo 		db "Eduardo Menzen"
+    	opJogar 		db "Jogar"
+    	opSair 			db "Sair"	
+	stringNumeros		db "0123456789"
+	tituloMatrizNavios 	db "Matriz de Navios"
+	tituloMatrizTiros 	db "Matriz de Tiros"
+	texConfig 		db "Digite a posicao do"
+	texVoce           	db "Voce"
+	texTiros          	db  "Tiros:"
+	texAcertos        	db  "Acertos:" 
+	texAfundados      	db  "Afundados:"
+	texComputador     	db  "Computador:"
+	texUltimoTiro     	db  "Ultimo Tiro:"
+	textPosicao		db	"Posicao:"
+	textMensagens		db	"Mensagens:"
+	vetquadrados        	db 10 dup(254,' ')  ;quadrado + caractere nulo
 
 .code
 EMPILHATUDO macro
     push AX
-    push BX
+    push BX 
     push CX
     push DX
     push SI
@@ -26,6 +43,49 @@ DESEMPILHATUDO macro
     pop AX
 endm
 
+SAIR_JOGO proc ; sai do jogo
+	mov al, 0
+	call MUDA_PAGINA    
+	mov AL, 00  ; Codigo de retorno
+	mov AH, 04CH; Servico de finalizar o programa
+	int 21H
+	ret      
+endp    
+
+
+LER_CHAR proc ; ler um caractere sem echo em AL (nao mostra na tela)
+    mov AH, 07H
+    int 21H   
+    ret       
+  endp
+  
+LER_CHAR_SJ proc  ; le o valor do teclado em AX
+    push AX
+	push BX
+    push DX 
+
+    ; posiciona o cursor
+    add DH, 11  ; linha
+    add DL, 30  ; coluna           
+    mov BH, 1   ; pagina
+    call MOV_CURSOR     
+
+LER_CHAR_SJ_LEITURA:       
+    call LER_CHAR	;ler o caractere
+
+    ; !!! VERIFICACAO !!!                    
+    cmp AL, 's'
+    jz SAIR_JOGO      
+
+    cmp AL, 'j'
+    jnz LER_CHAR_SJ_LEITURA
+
+    pop DX
+    pop BX
+	pop AX	
+    ret    
+endp
+
 MUDA_PAGINA proc ;muda para a página definida em AL
 	push AX
 	mov AH, 05h	;código da função: Selecione a página de exibição ativa
@@ -42,13 +102,6 @@ MOV_CURSOR proc ; move o cursor DH = linha, DL = coluna, BH = página
 	ret
 endp
 
-ESC_CHAR proc ; escreve um caractere em DL
-    push AX    ; salva o reg AX
-    mov AH, 2
-    int 21H
-    pop AX     ; restaura o reg AX
-    ret  
-endp 
 
 ESC_CHAR_10 proc ;escreve caracter de AL, BL: cor, BH: número da página, CX: repetições
 	push AX
@@ -73,93 +126,214 @@ ESC_STRING proc ;escreve string iniciada em ES:BP, comprimento CX, BH: número d
 	ret
 endp
 
-PRINT_LINHA_H proc	;escreve linha com tamanho CX
-	push AX
-	mov AL, 196 ;caracter -
-	call ESC_CHAR_10
-	add DL, CL	;encrementa coluna
-	call MOV_CURSOR
-	pop AX
-	ret
-endp
 
-PRINT_BORDAS proc ;escreve as bordas laterais para uma linha da tabela
+PRINT_BORDAS proc ;escreve as bordas laterais para uma linha da tabela com largura AL
 	push AX
 	push CX
+	push DX
+	
+	push AX
 	mov CX, 1	;para escrever apenas 1 vez cada caracter
 	mov AL, 179	;caracter |
 	call MOV_CURSOR
 	call ESC_CHAR_10
-	sub DL, 23 ;move para escrever a borda na primeira coluna
+	pop AX
+	inc AL
+	add DL, AL	;move para escrever a borda na segunda coluna
 	call MOV_CURSOR
+	mov AL, 179	;caracter |
 	call ESC_CHAR_10
+	
+	pop DX
 	pop CX
 	pop AX
 	ret
 endp
 
-PRINT_TABELA1 proc ;escreve a TABELA1 tendo como posição incial a linha DH e a coluna DL
-	EMPILHATUDO
 
-	;mov BH, AL	;grava a página ativa
-	;mov BL, 7 	;cor: cinza claro
-	mov CX, 1 	;número de repeticoes
+PRINT_LINHA_SUPERIOR proc	;escreve linha central da tabela na iniciando na posição DH: linha, DL: coluna, AX: largura da linha - 2(extremos) 
+	push AX
+	push CX
+	push DX
+	
+	push AX		;salva largura da linha
 	mov AL, 218 ;canto superior esquerdo
+	mov CX, 1 	;número de repeticoes
 	call MOV_CURSOR
 	call ESC_CHAR_10
-	inc DL		;incrementa coluna
+	inc DL
 	call MOV_CURSOR
-	mov CX, 22 	;número de repeticoes (tamanho da linha)
-	call PRINT_LINHA_H
+	pop AX		
+	add DL, AL  ;seta a coluna para escrever o canto direito
+	mov CX, AX	;número de repeticoes (largura da linha sem os extremos)
+	mov AL, 196 ;caracter -
+	call ESC_CHAR_10
+	call MOV_CURSOR	;move para a posição setada anteriormente
 	mov AL, 191 ;canto superior direito
 	mov CX, 1 	;número de repeticoes
 	call ESC_CHAR_10
-	inc DH
-	call PRINT_BORDAS
-	inc DH
-	mov AL, 195 ;canto central direito
+	
+	pop DX
+	pop CX
+	pop AX
+	ret
+endp
+
+PRINT_LINHA_CENTRAL proc	;escreve linha central na tabela, iniciando na posição DH: linha, DL: coluna, AX: largura da linha - 2(extremos)  
+	push AX
+	push CX
+	push DX
+	
+	push AX		;salva largura da linha
+	mov AL, 195 ;canto central esquerdo
+	mov CX, 1 	;número de repeticoes
 	call MOV_CURSOR
 	call ESC_CHAR_10
-	inc DL		;incrementa coluna
+	inc DL
 	call MOV_CURSOR
-	mov CX, 22 	;número de repeticoes (tamanho da linha)
-	call PRINT_LINHA_H
-	mov AL, 180 ;canto central esquerdo
+	pop AX		
+	add DL, AL  ;seta a coluna para escrever o canto direito
+	mov CX, AX	;número de repeticoes (largura da linha sem os extremos)
+	mov AL, 196 ;caracter -
+	call ESC_CHAR_10
+	call MOV_CURSOR	;move para a posição setada anteriormente
+	mov AL, 180 ;canto central direito
 	mov CX, 1 	;número de repeticoes
 	call ESC_CHAR_10
 	
-	mov CX, 11
+	pop DX
+	pop CX
+	pop AX
+	ret
+endp
+
+PRINT_LINHA_INFERIOR proc	;escreve linha central da tabela na iniciando na posição DH: linha, DL: coluna, AX: largura da linha - 2(extremos) 
+	push AX
+	push CX
+	push DX
+	
+	push AX		;salva largura da linha
+	mov AL, 192 ;canto inferior esquerdo
+	mov CX, 1 	;número de repeticoes
+	call MOV_CURSOR
+	call ESC_CHAR_10
+	inc DL
+	call MOV_CURSOR
+	pop AX		
+	add DL, AL  ;seta a coluna para escrever o canto direito
+	mov CX, AX	;número de repeticoes (largura da linha sem os extremos)
+	mov AL, 196 ;caracter -
+	call ESC_CHAR_10
+	call MOV_CURSOR
+	mov AL, 217 ;canto inferior direito
+	mov CX, 1 	;número de repeticoes
+	call ESC_CHAR_10
+	
+	pop DX
+	pop CX
+	pop AX
+	ret
+endp
+
+PRINT_RETANGULO proc ;escreve um retangulo de largura AX e altura CX, tendo como posição incial a linha DH e a coluna DL
+	push AX
+	push CX
+	push DX
+	
+	call PRINT_LINHA_SUPERIOR
 	BORDAS_CENTRO:
 		inc DH
 		call PRINT_BORDAS
-		add DL, 23
 	loop BORDAS_CENTRO
+	call PRINT_LINHA_INFERIOR
 	
-	mov CX, 1 	;número de repeticoes
-	sub DL, 23 	;coluna de base
-	inc DH
-	mov AL, 192 ;canto inferior direito
-	call MOV_CURSOR
-	call ESC_CHAR_10
-	inc DL		;incrementa coluna
-	call MOV_CURSOR
-	mov CX, 22 	;número de repeticoes (tamanho da linha)
-	call PRINT_LINHA_H
-	mov AL, 217 ;canto inferior esquerdo
-	mov CX, 1 	;número de repeticoes
-	call ESC_CHAR_10
+	pop DX
+	pop CX
+	pop AX
+	ret
+endp
+
+PRINT_TELA_INICIO proc
+	push AX
+	push BX
+	push CX
+	push DX
+	push BP
 	
-	sub DH, 11	;linha
-	sub DL, 20 	;coluna
+	mov BH, 1	;define o numero da página
+	mov DH, 1	;linha inicial
+	mov DL, 28 	;coluna inicial
+	mov BL, 7 	;cor: cinza claro
+	mov CX, 14  ;altura da retângulo
+	mov AX, 21  ;largura da retângulo
+	call PRINT_RETANGULO
+	
+	inc DH		;linha
+	add DL, 7   ;coluna
+	mov CX, 7 	;tamanho da string
+	mov BP, offset nomeJogo1 
+	call ESC_STRING  
+	
+	inc DH		;linha inicial
+	inc DL		;coluna inicial
+	mov CX, 5 	;tamanho da string
+	mov BP, offset nomeJogo2 
+	call ESC_STRING
+        
+	add DH, 3	;linha inicial 	
+	sub DL, 6   ;coluna inicial
+	mov CX, 8 	;tamanho da string
+	mov BP, offset textAutores  	   	
+	call ESC_STRING 	
+
+	; MOSTRA NOME ANDERSON IMPERATORI        
+	inc DH		;linha inicial 	
+	mov CX, 19 	;tamanho da string
+	mov BP, offset nomeAnderson  	   	
+	call ESC_STRING 
+	
+	; MOSTRA NOME EDUARDO MENZEN
+	inc DH		;linha inicial
+	mov CX, 14 	;tamanho da string
+	mov BP, offset nomeEduardo  	   	
+	call ESC_STRING
+	
+	; MOSTRA AS OPCOES  
+	;jogar
+	add DH, 3	;linha inicial 	
+	mov CX, 5 	;tamanho da string
+	mov BP, offset opJogar  	   	
+	call ESC_STRING  	
+	; sair
+	inc DH		;linha inicial   	
+	mov CX, 4 	;tamanho da string
+	mov BP, offset opSair  	   	
+	call ESC_STRING 
+	
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	ret
+endp
+
+ESC_NUMEROS proc ;escreve os numeros tendo como posição incial a linha DH e a coluna DL
+	push AX
+	push CX
+	push DX
+	push SI
+		
+	add DH, 3	;ajusta linha
+	add DL, 3 	;ajusta coluna
 	call MOV_CURSOR
 	mov CX, 10
-	mov SI,OFFSET vetNumeros	
+	mov SI,OFFSET stringNumeros	
 	ESC_NUM_HOR:	;escreve números horizontais
 		push CX
 		mov CX, 1
-        mov AL, [SI]
+		lodsb
         call ESC_CHAR_10
-		inc SI
 		inc DL
 		inc DL
 		call MOV_CURSOR
@@ -170,83 +344,205 @@ PRINT_TABELA1 proc ;escreve a TABELA1 tendo como posição incial a linha DH e a
 	sub DL, 22 	;coluna
 	call MOV_CURSOR
 	mov CX, 10
-	mov SI,OFFSET vetNumeros	
+	mov SI,OFFSET stringNumeros	
 	ESC_NUM_VER:	;escreve números verticais
 		push CX
 		mov CX, 1
-        mov AL, [SI]
+        lodsb
         call ESC_CHAR_10
-		inc SI
 		inc DH
 		call MOV_CURSOR
 		pop CX
 	loop ESC_NUM_VER
 	
-	DESEMPILHATUDO
-	ret
-endp
-
-PRINT_TABELA2 proc ;escreve a TABELA2 tendo como posição incial a linha DH e a coluna DL
-	EMPILHATUDO
-	
-	;mov BH, AL	;grava a página ativa
-	;mov BL, 7 	;cor: cinza claro
-	mov CX, 1 	;número de repeticoes
-	mov AL, 218 ;canto superior esquerdo
-	call MOV_CURSOR
-	call ESC_CHAR_10
-	inc DL		;incrementa coluna
-	call MOV_CURSOR
-	mov CX, 22 	;número de repeticoes (tamanho da linha)
-	call PRINT_LINHA_H
-	mov AL, 191 ;canto superior direito
-	mov CX, 1 	;número de repeticoes
-	call ESC_CHAR_10
-	mov CX, 19 	;tamanho da string
-	inc DH
-	mov BP, offset textoConfig
-	sub DL, 21
-	call ESC_STRING
-	add DL, 21
-	mov CX, 1 	;número de repeticoes
-	call PRINT_BORDAS
-	add DL, 23
-	inc DH
-	call PRINT_BORDAS
-	add DL, 23
-	inc DH
-	call PRINT_BORDAS
-	inc DH
-	mov AL, 192 ;canto inferior direito
-	call MOV_CURSOR
-	call ESC_CHAR_10
-	inc DL		;incrementa coluna
-	call MOV_CURSOR
-	mov CX, 22 	;número de repeticoes (tamanho da linha)
-	call PRINT_LINHA_H
-	mov AL, 217 ;canto inferior esquerdo
-	mov CX, 1 	;número de repeticoes
-	call ESC_CHAR_10
-	
-	DESEMPILHATUDO
+	pop SI
+	pop DX
+	pop CX
+	pop AX
 	ret
 endp
 
 PRINT_TELA_CONFIG proc
-	EMPILHATUDO
-	mov BH, AL	;grava a página ativa
+	push AX
+	push BX
+	push CX
+	push DX
+	push BP
+	
+	mov BH, 2	;define o numero da página
 	mov DH, 1	;linha inicial
 	mov DL, 28 	;coluna inicial
 	mov BL, 7 	;cor: cinza claro
-	call PRINT_TABELA1	
-	add DH, 16	;linha inicial
-	call PRINT_TABELA2
-	sub DH, 15	;linha inicial
+	mov CX, 14  ;altura da retângulo
+	mov AX, 21  ;largura da retângulo
+	call PRINT_RETANGULO
+	call ESC_NUMEROS
+	add DH, 2
+	call PRINT_LINHA_CENTRAL
+	dec DH	;linha
 	add DL, 4
 	mov CX, 16 	;tamanho da string
-	mov BP, offset tituloConfigIniciAL
+	mov BP, offset tituloMatrizNavios
+	call ESC_STRING
+	sub DL, 4
+	add DH, 14	;linha
+	mov CX, 4  ;altura da retângulo
+	call PRINT_RETANGULO
+	inc DH
+	add DL, 2
+	mov CX, 19 	;tamanho da string
+	mov BP, offset texConfig
 	call ESC_STRING
 	
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	ret
+endp
+
+PRINT_QUADRADOS_VERDES proc
+	EMPILHATUDO 
+	
+    mov BL, 2 	;cor: verde
+    add DH, 4   ;ajusta linha
+    add DL, 3   ;ajusta coluna
+    mov CX, 10
+    mov BP, offset vetquadrados
+	LACO_QUADRADOS:
+        push CX
+        call MOV_CURSOR
+    	mov CX, 20
+    	call ESC_STRING
+    	inc DH
+    	pop CX
+	loop LACO_QUADRADOS
+	
+	DESEMPILHATUDO
+	ret
+endp
+
+PRINT_TELA_JOGO proc
+	EMPILHATUDO
+	
+	mov BH, 3
+	mov DH, 1	;linha inicial
+	mov DL, 1 	;coluna inicial
+	mov BL, 7 	;cor: cinza claro 
+	
+	;desenhar tabela de tiros
+	call PRINT_QUADRADOS_VERDES
+	mov CX, 14  ;altura da retângulo
+	mov AX, 21  ;largura da retângulo
+	call PRINT_RETANGULO
+	call ESC_NUMEROS
+	
+	;desenhar tabela de navios
+	add DL, 24  ;coluna
+	call PRINT_RETANGULO
+	call ESC_NUMEROS
+	inc DH
+	sub DL, 20
+	mov CX, 15 	;tamanho da string
+	mov BP, offset tituloMatrizTiros
+	call ESC_STRING
+	inc DH
+	sub DL, 4
+	call PRINT_LINHA_CENTRAL
+	add DL, 24
+	call PRINT_LINHA_CENTRAL
+	dec DH
+	mov CX, 16	;tamanho da string
+	add DL, 4
+	mov BP, offset tituloMatrizNavios
+	call ESC_STRING
+	
+	;desenhar tabela de resultados
+	inc DH
+	add DL, 21
+	mov CX, 11  ;altura da retângulo
+	mov AX, 18  ;largura da retângulo
+	call PRINT_RETANGULO
+	
+	;escrever os textos dentro da tabela de resultados
+	inc DH  	;linha
+	inc DL  	;coluna
+	mov CX, 4	;tamanho da string
+	mov BP, offset texVoce
+	call ESC_STRING
+	
+	add DH, 4	;linha
+	dec DL  	;coluna
+	call PRINT_LINHA_CENTRAL
+	
+	inc DH  	;linha
+	inc DL  	;coluna
+	mov CX, 11	;tamanho da string
+	mov BP, offset texComputador
+	call ESC_STRING	
+	
+	sub DH, 6    ;linha
+	add DL, 3 	;coluna
+	mov CX, 2	
+	LACO:
+		push CX
+		add DH,2  	;linha
+		mov CX, 6	;tamanho da string
+		mov BP, offset texTiros
+		call ESC_STRING
+		inc DH  	;linha
+		mov CX, 8	;tamanho da string
+		mov BP, offset texAcertos
+		call ESC_STRING
+		inc DH  	;linha
+		mov CX, 10	;tamanho da string
+		mov BP, offset texAfundados
+		call ESC_STRING
+		inc DH
+		pop CX
+	loop LACO
+	mov CX, 12	;tamanho da string
+	mov BP, offset texUltimoTiro
+	call ESC_STRING
+    
+    add DH, 4   ;linha
+    mov DL, 1 	;coluna inicial
+    mov CX, 3  	;altura da retângulo
+	mov AX, 67  ;largura da retângulo
+	call PRINT_RETANGULO
+	
+	inc DH		;linha
+	inc DL      ;coluna
+	mov CX, 8	;tamanho da string
+	mov BP, offset textPosicao
+	call ESC_STRING
+	add DL, 22  ;coluna
+	mov CX, 10	;tamanho da string
+	mov BP, offset textMensagens
+	call ESC_STRING
+	
+	dec DH      ;linha
+	dec DL      ;coluna
+	call MOV_CURSOR       
+	mov AL, 194 ;caracter  _|_ invertido
+	xor CX, CX
+	inc CX
+	call ESC_CHAR_10
+	
+	inc DH      ;linha
+	call MOV_CURSOR
+	mov AL, 179	;caracter |
+	call ESC_CHAR_10
+	inc DH      ;linha
+	call MOV_CURSOR
+	call ESC_CHAR_10	
+
+	inc DH      ;linha
+	call MOV_CURSOR
+	mov AL, 193	;caracter _|_
+	call ESC_CHAR_10	
+    
 	DESEMPILHATUDO
 	ret
 endp
@@ -258,15 +554,24 @@ INICIO:	mov AX, @data ; carrega valor inicial da stack
 	mov AL, 03h ;código do modo desejado neste caso 80x25  8x8  texto 16 cores
     int 10h
 	
+	mov AL, 1	;define o número da página
+	call MUDA_PAGINA
+	call PRINT_TELA_INICIO
+	
+	; chama prog ler S ou J  
+	call LER_CHAR_SJ
+	
 	mov AL, 2	;define o número da página
 	call MUDA_PAGINA
 	call PRINT_TELA_CONFIG
 	
+	call LER_CHAR
 	
+	mov AL, 3	;define o número da página
+	call MUDA_PAGINA
+	call PRINT_TELA_JOGO
 	
+	call LER_CHAR
 	
-	
-   mov AL, 00  ; Codigo de retorno
-   mov AH, 04CH; Servico de finalizar o programa
-   int 21H	
+	call SAIR_JOGO ; finaliza o jogo
 end INICIO
